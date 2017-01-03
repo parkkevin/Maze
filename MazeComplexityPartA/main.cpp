10078eb09d69599b4e636b4f83f3c71407d87114
Kevin Park <Kevin@Kevins-Macbook-Pro.local>
2017-01-02 21:46:36 -0800

    folder added

---
+++ MazeComplexityPartA/main.cpp
@@ -0,0 +1,48 @@
+#include "MazeComplexityPartA.h"
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+    MazeComplexityPartA mc;
+    int row, col;
+    
+    mc.print();
+    cout << endl;
+    
+    cout << "Enter a row number of a starting point in range from 0 to 9: ";
+    cin >> row;
+    cout << endl;
+    cout << "Enter a column number of a starting point in range from 0 to 9: ";
+    cin >> col;
+    cout << endl;
+    
+    mc.fillBlack(row, col);
+    mc.print();
+    cout << endl;
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
